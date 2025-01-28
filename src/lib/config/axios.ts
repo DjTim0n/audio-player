5588490b785df440c31f4ecfb169671d50e4d1cc
Tim <djtimon100@gmail.com>
2025-01-28 21:11:31 +0500

    Update Docker configuration and refactor API base URL to use environment variable

--- src/lib/config/axios.ts
+++ src/lib/config/axios.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export const apiWithAuth = axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
